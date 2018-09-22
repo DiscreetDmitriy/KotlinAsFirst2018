28906e0ea9dfb49834e09e6649a7ec9fa7dcf6bc
DiscreetDmitriy <26caesar0800>
2018-09-22 18:47:14 +0300

    push

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -51,7 +51,7 @@ fun daysInMonth(month: Int, year: Int): Int {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = (x1 - r1 >= x2 - r2) && (x1 + r1 <= x2 + r2) && (y1 - r1 >= y2 - r2) && (y1 + r1 <= y2 + r2)
+                 x2: Double, y2: Double, r2: Double): Boolean = ((x1 - r1 >= x2 - r2) && (x1 + r1 <= x2 + r2) && (y1 - r1 >= y2 - r2) && (y1 + r1 <= y2 + r2))
 
 /**
  * Средняя

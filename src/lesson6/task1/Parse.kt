3b426a73361a43bb4b3290094c2987c23d497b04
a <b>
2018-11-25 18:05:48 +0300

    fix 3

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -270,7 +270,7 @@ fun mostExpensive(description: String): String {
 fun fromRoman(roman: String): Int {
     val signToNumber = mapOf("M" to 1000, "CM" to 900, "D" to 500, "CD" to 400, "C" to 100,
             "XC" to 90, "L" to 50, "XL" to 40, "X" to 10, "IX" to 9, "V" to 5, "IV" to 4, "I" to 1)
-    if (Regex("[^IVXLCDM]+").containsMatchIn(roman)) return -1
+    if (Regex("[^IVXLCDM]").containsMatchIn(roman) || roman == "") return -1
 
     return Regex("CM|CD|XC|XL|IX|IV|M|D|C|L|X|V|I")
             .findAll(roman)
